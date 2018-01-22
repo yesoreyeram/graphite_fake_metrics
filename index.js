389850589658aae70cea66e196333049a3c0a8c7
Sriramajeyam Sugumaran <yesoreyeram@gmail.com>
2018-01-22 11:34:35 +0000

    Added environment to the metrics

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var _ = require("lodash");
 const INTERVAL_IN_SECONDS = 60;
 const config = {
+    environments: ["prod", "preprod", "test", "dev"],
     server_types: ["web", "app", "db"],
     number_of_servers: 3,
     application_type: ["orders", "payments", "customercare", "backoffice"],
@@ -8,24 +9,28 @@ const config = {
 }
 var main = function () {
     var metrics = {};
-    _.each(config.server_types, (server_type) => {
-        _.each(_.range(config.number_of_servers), (server_id) => {
-            _.set(metrics, "server_stats." + server_type + "_" + server_id + ".cpu.usage", Math.round(Math.random() * 100), Object);
-            _.set(metrics, "server_stats." + server_type + "_" + server_id + ".mem.usage", Math.round(Math.random() * 100), Object);
-            _.set(metrics, "server_stats." + server_type + "_" + server_id + ".disk.c.freespace", Math.ceil(Math.random() * 100) * 1024 * 1024, Object);
-            _.set(metrics, "server_stats." + server_type + "_" + server_id + ".disk.d.freespace", Math.ceil(Math.random() * 100) * 1024 * 1024, Object);
-            _.set(metrics, "server_stats." + server_type + "_" + server_id + ".disk.e.freespace", Math.ceil(Math.random() * 100) * 1024 * 1024, Object);
-        });
-    })
-    _.each(config.application_type, (app_type) => {
-        _.set(metrics, "app_stats." + app_type + ".requests_per_second", Math.random() * 100, Object);
-        _.set(metrics, "app_stats." + app_type + ".requests_passed", Math.round(Math.random() * 100), Object);
-        _.set(metrics, "app_stats." + app_type + ".requests_failed", Math.round(Math.random() * 100), Object);
-        _.each((config.application_metrics).split(","), (metric) => {
-            _.set(metrics, "app_stats." + app_type + ".stat." + metric, Math.round(Math.random() * 100), Object);
-        });
+    _.each(config.environments, (env) => {
+        _.each(config.server_types, (server_type) => {
+            _.each(_.range(config.number_of_servers), (server_id) => {
+                _.set(metrics, env + ".server_stats." + server_type + "_" + server_id + ".cpu.usage", Math.round(Math.random() * 100), Object);
+                _.set(metrics, env + ".server_stats." + server_type + "_" + server_id + ".mem.usage", Math.round(Math.random() * 100), Object);
+                _.set(metrics, env + ".server_stats." + server_type + "_" + server_id + ".disk.c.freespace", Math.ceil(Math.random() * 100) * 1024 * 1024, Object);
+                _.set(metrics, env + ".server_stats." + server_type + "_" + server_id + ".disk.d.freespace", Math.ceil(Math.random() * 100) * 1024 * 1024, Object);
+                _.set(metrics, env + ".server_stats." + server_type + "_" + server_id + ".disk.e.freespace", Math.ceil(Math.random() * 100) * 1024 * 1024, Object);
+            });
+        })
+        _.each(config.application_type, (app_type) => {
+            _.set(metrics, env + ".app_stats." + app_type + ".requests_per_second", Math.random() * 100, Object);
+            _.set(metrics, env + ".app_stats." + app_type + ".requests_passed", Math.round(Math.random() * 100), Object);
+            _.set(metrics, env + ".app_stats." + app_type + ".requests_failed", Math.round(Math.random() * 100), Object);
+            _.each((config.application_metrics).split(","), (metric) => {
+                _.set(metrics, env + ".app_stats." + app_type + ".stat." + metric, Math.round(Math.random() * 100), Object);
+            });
+        })
     })
     console.log(JSON.stringify(metrics));
 }
 main();
-setInterval(function () {main()}, INTERVAL_IN_SECONDS * 1000)
+setInterval(function () {
+    main()
+}, INTERVAL_IN_SECONDS * 1000)
